c54aac73f1ddf980b28ad1a2935a6be3e0d3531c
masaki420 <zyab5275@gmail.com>
2021-06-02 05:33:31 +0900

    ヒーロー詳細画面編集

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,9 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+	{id: 6, name: 'syun' },
+	{id: 2, name: 'yuko' },
+	{id: 3, name: 'maru' },
+	{id: 4, name: 'michel' },
+	{id: 5, name: 'ayaka' },
+];
