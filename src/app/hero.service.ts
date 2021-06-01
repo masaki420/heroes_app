a905b736747277ce19648fa8dbbfbfcaacb01af4
masaki420 <zyab5275@gmail.com>
2021-06-02 07:32:32 +0900

    app completed

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -54,6 +54,47 @@ getHero(id: number): Observable<Hero> {
   return this.http.get<Hero>(url).pipe(
     tap(_ => this.log(`fetched hero id=${id}`)),
     catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
+  }
+
+  updateHero(hero: Hero): Observable<any> {
+    return this.http.put(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap(_ => this.log(`updated hero id=${hero.id}`)),
+      catchError(this.handleError<any>('updateHero'))
+    );
+  }
+
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type':'application/json'})
+  };
+
+  /** POST: サーバーに新しいヒーローを登録する */
+addHero(hero: Hero): Observable<Hero> {
+  return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+    tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+    catchError(this.handleError<Hero>('addHero'))
+  );
+}
+
+  /** DELETE: サーバーからヒーローを削除 */
+deleteHero(id: number): Observable<Hero> {
+  const url = `${this.heroesUrl}/${id}`;
+
+  return this.http.delete<Hero>(url, this.httpOptions).pipe(
+    tap(_ => this.log(`deleted hero id=${id}`)),
+    catchError(this.handleError<Hero>('deleteHero'))
+  );
+}
+
+/* 検索語を含むヒーローを取得する */
+searchHeroes(term: string): Observable<Hero[]> {
+  if (!term.trim()) {
+    // 検索語がない場合、空のヒーロー配列を返す
+    return of([]);
+  }
+  return this.http.get<Hero[]>(`${this.heroesUrl}/?name=${term}`).pipe(
+    tap(_ => this.log(`found heroes matching "${term}"`)),
+    catchError(this.handleError<Hero[]>('searchHeroes', []))
   );
 }
 }
